9b949bc5df7811b1f84d80343c7d76817ed50817
wangxinxinsq <wxx@e7chong.com>
2016-08-12 10:10:47 +0800

    修改代码

--- encryption_decryption/do_sha1.py
+++ encryption_decryption/do_sha1.py
@@ -1,14 +1,15 @@
+# -*- coding: utf-8 -*-
 import hashlib
 
 
-#sha1加密(小写)
+# sha1加密(小写)
 def to_sha1(part):
     sha1 = hashlib.sha1()
     sha1.update(part.encode())
     return sha1.hexdigest()
 
 
-#sha1加密(大写)
+# sha1加密(大写)
 def sha1_signature(part):
     sha1 = hashlib.sha1()
     sha1.update(part.encode())
